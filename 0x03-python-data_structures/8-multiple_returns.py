cd1c18b558c9f69afcef4b0dde9d46850b47ac5e
Sarah-Gad <sarahgad298@gmail.com>
2024-01-08 14:31:25 -0800

    edit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     thelen = len(sentence)
-    if sentence is None:
+    if sentence is None and thelen < 0:
         first = None
     else:
         first = sentence[0]

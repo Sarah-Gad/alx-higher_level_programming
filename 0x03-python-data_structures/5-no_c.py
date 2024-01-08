a042f780fa1e49d39443749b3b366f8c588bbbf9
Sarah-Gad <sarahgad298@gmail.com>
2024-01-08 08:02:06 -0800

    edit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    nwlst = list(my_string)
-    i = 0
-    while i < len(nwlst):
-        if nwlst[i] == 'c' or nwlst[i] == 'C':
-            nwlst.pop(i)
-        else:
-            i += 1
-    return (''.join(nwlst))
+    return "".join(c for c in my_string if c not in ('c', 'C'))

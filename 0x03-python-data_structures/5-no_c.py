796411edce6db151a02690f8183b80b38ba803cc
Sarah-Gad <sarahgad298@gmail.com>
2024-01-08 07:37:40 -0800

    edit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    nwstr = ""
-    for i in my_string:
-        if i.lower() not in {'c'}:
-            nwstr = nwstr + i
-    return nwstr
+    if my_string is None:
+        my_string = ""
+    else:
+        convrtd = list(my_string)
+        thelen = len(convrtd)
+        i = thelen - 1
+        while i >= 0:
+            if convrtd[i] == "c" or convrtd[i] == "C":
+                convrtd.pop(i)
+            i -= 1
+        return ("".join(convrtd))

88c53b427e3076d29510ec83cb4eda114b230075
Sarah-Gad <sarahgad298@gmail.com>
2024-01-08 16:32:59 -0800

    completed task n.11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    thelen = len(my_list)
+    if idx < 0 or idx > thelen:
+        return my_list
+    else:
+        del my_list[idx]
+    return my_list

2181134c0b2337ca85741c72158630ede0660fb6
Sarah-Gad <sarahgad298@gmail.com>
2024-01-10 15:42:26 -0800

    completed task n.1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    nw_list = my_list.copy()
+    for i in nw_list:
+        if i == search:
+            theind = my_list.index(i)
+            my_list.remove(i)
+            my_list.insert(theind, replace)
+    return nw_list

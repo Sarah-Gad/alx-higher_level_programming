7bd2404018a44081231b7873e0b9263802821d4e
Sarah-Gad <sarahgad298@gmail.com>
2024-01-10 15:47:42 -0800

    edit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     nw_list = my_list.copy()
     for i in nw_list:
         if i == search:
-            theind = my_list.index(i)
-            my_list.pop(theind)
-            my_list.insert(theind, replace)
+            theind = nw_list.index(i)
+            nw_list.pop(theind)
+            nw_list.insert(theind, replace)
     return nw_list

76d370eb7c05a8e48b05fce68e094b4ab31e5478
Sarah-Gad <sarahgad298@gmail.com>
2024-01-23 15:19:55 -0800

    complete dtask n.2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This is a definition of a class square"""
+
+
+class Square:
+    """the code inside the class"""
+    def __init__(self, size=0):
+        """This is the constuctor function, instance.
+
+        Args:
+        size(int): the acual size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size

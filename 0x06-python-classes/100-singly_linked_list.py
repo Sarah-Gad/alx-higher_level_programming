e55d8ffc98b687b89d3f92eb6d2c372bf4a4c520
Sarah-Gad <sarahgad298@gmail.com>
2024-01-23 17:53:52 -0800

    edit

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -35,38 +35,40 @@ class Node:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
-    class SinglyLinkedList:
-        """ this is the singly linked list"""
-        def __init__(self):
-            """ start the singly linked list"""
-            self.__head = None
 
-        def __str__(self):
-            """ the string of the list"""
-            string = ""
-            start = self.__head
-            while start:
-                if start is not self.__head:
-                    string += '\n'
-                string += str(start.data)
-                start = start.next_node
-            return string
+class SinglyLinkedList:
+    """ this is the singly linked list"""
 
-        def sorted_insert(self, value):
-            """ insert the new nodde"""
-            new = Node(value)
-            if not self.__head:
-                self.__head = new
-                return
-            prev = None
-            temp = self.__head
-            while temp:
-                if value < temp.data:
-                    break
-                prev = temp
-                temp = temp.next_node
-            if prev:
-                prev.next_node = new
-            else:
-                self.__head = new
-            new.next_node = temp
+    def __init__(self):
+        """ start the singly linked list"""
+        self.__head = None
+
+    def __str__(self):
+        """ the string of the list"""
+        string = ""
+        start = self.__head
+        while start:
+            if start is not self.__head:
+                string += '\n'
+            string += str(start.data)
+            start = start.next_node
+        return string
+
+    def sorted_insert(self, value):
+        """ insert the new nodde"""
+        new = Node(value)
+        if not self.__head:
+            self.__head = new
+            return
+        prev = None
+        temp = self.__head
+        while temp:
+            if value < temp.data:
+                break
+            prev = temp
+            temp = temp.next_node
+        if prev:
+            prev.next_node = new
+        else:
+            self.__head = new
+        new.next_node = temp

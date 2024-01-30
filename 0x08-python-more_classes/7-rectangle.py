b53663082a8610a6c642ce27d331c4e7e4d2e36f
Sarah-Gad <sarahgad298@gmail.com>
2024-01-29 18:28:45 -0800

    edit

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -74,4 +74,8 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
+        Rectangle.number_of_instanceis -= 1
+
+    @classmethod
+    def set_symbol(cls, symbol):
+        cls.print_symbol = symbol

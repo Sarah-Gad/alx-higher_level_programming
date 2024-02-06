0928d117aa3a1e4e771c28f460bd559b65b47a5b
Sarah-Gad <sarahgad298@gmail.com>
2024-02-05 19:03:41 -0800

    deleted

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 
-"""Class MyList that inherits from list"""
+"""Class MyList that inheritslist"""
 
 
 class MyList(list):
-    """Class MyList that inherits from list"""
+    """Class MyList that inheritslist"""
 
     def print_sorted(self):
         """Print the sorted list"""

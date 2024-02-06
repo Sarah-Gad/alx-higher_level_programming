da97832aa760d385081826120aceb66bd253b49c
Sarah-Gad <sarahgad298@gmail.com>
2024-02-06 15:16:10 -0800

    complete dtask n.6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this module manipulates  a file"""
+
+
+def load_from_json_file(filename):
+    """This method creates an Object from a “JSON file”"""
+    import json
+    with open(filename, 'r') as myfile:
+        json.load(myfile)

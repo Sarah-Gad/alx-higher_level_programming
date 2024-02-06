5bfe46606092e5c748b569c477489b4a912eb783
Sarah-Gad <sarahgad298@gmail.com>
2024-02-06 13:51:50 -0800

    edit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""This module conyains a function of method to read from a file"""
+
+
 def read_file(filename=""):
+    """The method to read and print"""
     with open(filename, 'r', encoding="utf-8") as myfile:
         read = myfile.read()
         print(read, end="")

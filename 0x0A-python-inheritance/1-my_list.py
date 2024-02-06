107cdeb6ce95dd3b7e376ebb9789066741fbab31
Sarah-Gad <sarahgad298@gmail.com>
2024-02-05 19:11:48 -0800

    ok

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-
-"""This is a module for MyList"""
+'''Module for MyList class.'''
 
 
 class MyList(list):
-    """This is MyList class"""
+    '''Custom MyList class.'''
     def print_sorted(self):
-        """The method for printing"""
+        '''Method for printing sorted list.'''
         print(sorted(self))

93a04c2eed94827a05486fc05b2b102f80c10b49
Sarah-Gad <sarahgad298@gmail.com>
2024-02-06 18:05:29 -0800

    .-.

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3/
 """This module usues json strings"""
-import json
 
 
 def class_to_json(obj):
     """This method returns a dictionary
     description with simple data structure"""
-    return obj.__dict__
+    mydict = {}
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            mydict[key] = value
+    return mydict

f8065c7a0dfaa5f4959e4db0a16bcbe0719f95d8
Sarah-Gad <sarahgad298@gmail.com>
2024-02-06 18:01:45 -0800

    edit

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,8 +6,4 @@ import json
 def class_to_json(obj):
     """This method returns a dictionary
     description with simple data structure"""
-    mydict = {}
-    for key, value in obj.__dict__.items():
-        if isinstance(value, (list, dict, str, int, bool)):
-            mydict[key] = value
-    return mydict
+    return obj.__dict__

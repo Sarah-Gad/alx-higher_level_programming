5e0eea6d41698b93a3847a448082be8fcadab81f
Sarah-Gad <sarahgad298@gmail.com>
2024-02-12 18:56:27 -0800

    complete dtask n.10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module define a square class"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """This class is a child of the rectangle class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)

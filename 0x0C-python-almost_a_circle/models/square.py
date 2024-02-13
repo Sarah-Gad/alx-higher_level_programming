79096da2ff3f1070d5ef29e4a6c872273934ec9d
Sarah-Gad <sarahgad298@gmail.com>
2024-02-12 19:27:51 -0800

    complete dtask n .12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,29 @@ class Square(Rectangle):
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
                 self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """a method that assigns the attributes"""
+        if (len(args) != 0):
+            i = 0
+            for arg in args:
+                if i == 0:
+                    self.id = arg
+                if i == 1:
+                    self.width = self.height = arg
+                if i == 2:
+                    self.x = arg
+                if i == 3:
+                    self.y = arg
+                i += 1
+
+        elif (len(kwargs) != 0):
+            for k, v in kwargs.items():
+                if k == "size":
+                    self.width = self.height = v
+                if k == "id":
+                    self.id = v
+                if k == "x":
+                    self.x = v
+                if k == "y":
+                    self.y = v

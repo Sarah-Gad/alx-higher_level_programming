c2681a5f85dc70a01cef3fb10734898cc0480498
Sarah-Gad <sarahgad298@gmail.com>
2024-02-12 16:58:04 -0800

    edit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,6 +3,8 @@
 
 
 from models.base import Base
+
+
 class Rectangle(Base):
     """This rectangle class is the child class of the Base class"""
     def __init__(self, width, height, x=0, y=0, id=None):

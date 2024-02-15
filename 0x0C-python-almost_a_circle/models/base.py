3caec63831eeb89fea409324448ee2b1eb394a6c
Sarah-Gad <sarahgad298@gmail.com>
2024-02-15 11:12:16 -0800

    complete dtask n.14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module contain the base class"""
 
+import json
+
 
 class Base:
     """This class manages id attribute"""
@@ -12,3 +14,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)

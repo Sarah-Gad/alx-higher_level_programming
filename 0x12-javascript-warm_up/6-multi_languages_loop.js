e866bb30aa56a8ceaf0276b6e28950339f4d0087
Sarah-Gad <sarahgad298@gmail.com>
2024-04-11 20:06:20 -0700

    edit

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+const sentss = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+for (let i = 0; i < sentss.length; i++) {
+  console.log(sentss[i]);
 }

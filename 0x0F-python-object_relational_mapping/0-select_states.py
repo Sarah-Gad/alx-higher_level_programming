e083cce891894180b424ee849d7c11b21b5a465f
Sarah-Gad <sarahgad298@gmail.com>
2024-04-14 18:40:00 -0700

    edit pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+"""
+This script lists all states from the database hbtn_0e_0_usa
+"""
 
 import sys
 import MySQLdb
 
+
 def main():
-    newcon = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    newcon = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
     newcurs = newcon.cursor()
     newcurs.execute(f"SELECT * FROM `states` ORDER BY `id` ASC")
     for row in newcurs:
@@ -12,6 +21,6 @@ def main():
     newcurs.close()
     newcon.close()
 
+
 if __name__ == "__main__":
     main()
-

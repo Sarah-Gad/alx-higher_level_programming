e3b010e79d74c49bb66eaf50d60125f4add53752
Sarah-Gad <sarahgad298@gmail.com>
2024-04-28 19:27:51 -0700

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This script sends a POST request to the passed URL """
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    thedata = {"email": sys.argv[2]}
+    r = requests.post(sysargv[1], data=thedata)
+    print(r.text)

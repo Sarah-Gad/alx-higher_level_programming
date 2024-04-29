0ffc7be1dff93390e2ec8fae1e4176736979e76a
Sarah-Gad <sarahgad298@gmail.com>
2024-04-28 18:11:32 -0700

    edit .-.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
         data = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(data)))
-        print("\t- content: {}".format(data))
+        print("\t- content: {!r}".format(data))
         print("\t- utf8 content: {}".format(data.decode("utf-8")))

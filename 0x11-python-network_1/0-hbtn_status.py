a935b0062bea72695bf1d147e4454883e2101d11
Sarah-Gad <sarahgad298@gmail.com>
2024-04-28 18:14:52 -0700

    .-.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """This module  fetches https://alx-intranet.hbtn.io/status"""
 
-import urllib.request
-
 
 if __name__ == "__main__":
+    import urllib.request
+
     with urllib.request.urlopen(
             "https://alx-intranet.hbtn.io/status") as response:
         data = response.read()

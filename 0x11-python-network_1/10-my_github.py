4a814698e34a9542e80f56001bf014a3afc9a43b
Sarah-Gad <sarahgad298@gmail.com>
2024-04-28 19:51:40 -0700

    .-.

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module takes the GitHub credentials (username and password)
+and uses the GitHub API to display the id"""
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))

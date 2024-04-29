1010c8b970a7ad61b97e76a45ef727502facd632
Sarah-Gad <sarahgad298@gmail.com>
2024-04-28 18:20:36 -0700

    .-.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """This module  fetches https://alx-intranet.hbtn.io/status"""
 
+import urllib.request
+
 
 if __name__ == "__main__":
-    import urllib.request
-
     with urllib.request.urlopen(
             "https://alx-intranet.hbtn.io/status") as response:
         data = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(data)))
-        print("\t- content: {!r}".format(data))
+        print("\t- content: {}".format(data))
         print("\t- utf8 content: {}".format(data.decode("utf-8")))

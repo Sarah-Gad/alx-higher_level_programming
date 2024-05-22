6bb45303a0b65e213d35c50734291807674584cc
Sarah-Gad <sarahgad298@gmail.com>
2024-05-22 04:06:27 +0300

    fixed semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,9 +3,8 @@
 This script will write a string to a file.
 */
 const writer = require('fs');
-writer.writeFile(process.argv[2], process.argv[3], 'utf8', (theerror)=>{
-    if (theerror){
-        console.error(theerror);
-        return;
-    }
-})
+writer.writeFile(process.argv[2], process.argv[3], 'utf8', (theerror) => {
+  if (theerror) {
+    console.error(theerror);
+  }
+});

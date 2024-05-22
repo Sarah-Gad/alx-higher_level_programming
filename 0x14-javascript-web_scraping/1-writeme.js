5296d156f9ed4f46511b8eb419096d9a07eb29e1
Sarah-Gad <sarahgad298@gmail.com>
2024-05-22 04:03:47 +0300

    completed task n.1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+This script will write a string to a file.
+*/
+const writer = require('fs');
+writer.writeFile(process.argv[2], process.argv[3], 'utf8', (theerror)=>{
+    if (theerror){
+        console.error(theerror);
+        return;
+    }
+})

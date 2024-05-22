6a21bd2684a94a45fc722e1286f39d08d87772f9
Sarah-Gad <sarahgad298@gmail.com>
2024-05-22 06:02:57 +0300

    complete dtask n.

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/*
+this script will get the contents of a webpage and stores it in a file.
+*/
+const req = require('request');
+const writer = require('fs');
+req(process.argv[2], { json: true }, (theerror, respo, body) => {
+  if (theerror) {
+    console.error(theerror);
+    return;
+  }
+  writer.writeFile(process.argv[3], body, 'utf8', (error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
+});
